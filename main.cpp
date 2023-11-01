171bd4b6cd580dd3737c6f5a7d464bbe845984de
laru <larape715@gmail.com>
2023-11-01 08:26:59 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ struct articulo {
     string nom_articulo;
     Cola<int> depositos;
 };
-void imprimir_articulos (Pila<articulo> a, int num);
+void imprimir_articulo (articulo art);
+void imprimir_pila (Pila<articulo> a);
 int main(int argc, char **argv) {
     clock_t begin;
     cout << "Comenzando a medir Tiempo\n" << endl;
@@ -24,20 +25,27 @@ int main(int argc, char **argv) {
     filename= argv[1];
     string linea;
     Pila<articulo > pila;
-    int num_depositos;
+    int num_depositos =0;
     fstream archivo;
     archivo.open("./" + filename);
+    if (!archivo.is_open()) {
+        cout << "Error al abrir el archivo" << endl;
+        throw 400; // Salir del programa con código de error
+    }
     getline(archivo, linea,',');
     getline(archivo, linea,',');
     getline(archivo, linea,',');
     string deposito;
 
     while (getline(archivo, deposito, ',')) {
+        cout << "Depósito: " << deposito << endl; // Imprimir cada depósito
+
         if (deposito == "\n") {
             break; // Si encuentra un salto de línea, terminar el bucle
         }
         num_depositos++;
     }
+    cout<< "num: "<< num_depositos;
 
         if (::strcmp(argv[2], "-total_art_dif" )==0){
             //linea de codigo para este argumento
@@ -54,12 +62,18 @@ int main(int argc, char **argv) {
                 }
             }
             cout << "La cantidad total de articulos es: " << cant_total_art << " articulos." << endl;
-            //linea de codigo para este argumento
         }else if (::strcmp(argv[2], "-min_stock" )==0){
+            string grupo_anterior;
             while (getline(archivo,linea)){
                 articulo a;
+
                 int n = stoi(argv[3]);
                 getline(archivo,a.grupo, ',' );
+                if (!a.grupo.empty()){
+                    grupo_anterior = a.grupo;
+                }else{
+                    a.grupo = grupo_anterior;
+                }
                 getline(archivo,a.cod_barras, ',' );
                 getline(archivo,a.nom_articulo, ',' );
                 int cant_stock=0;
@@ -76,8 +90,9 @@ int main(int argc, char **argv) {
                 if (cant_stock <= n){
                     pila.push(a);
                 }
-                imprimir_articulos(pila, n);
+                cout << "Listado de artículos con cantidad <= " << n<< " de stock: " << endl;
             }
+
         }else if (::strcmp(argv[2], "-min_stock_dep" )==0){
             //linea de codigo para este argumento
         }else if (::strcmp(argv[2], "-stock" )==0){
@@ -86,6 +101,8 @@ int main(int argc, char **argv) {
             //linea de codigo para este argumento
         }else if (::strcmp(argv[2], "-max_stock" )==0){
             //linea de codigo para este argumento
+        }else {
+            cout <<" No ingresó un parámetro correcto"<<endl;
         }
     clock_t end = clock();
 
@@ -95,10 +112,20 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-//funcion para imprimir art, usada en argumento -min_stock
-void imprimir_articulos (Pila<articulo> a, int num){
-    cout << "Listado de artículos con cantidad <= " << num << " de stock: " << endl;
+void imprimir_articulo (articulo art){
+    int i = 1;
+    cout<< "\nGrupo: "<<art.grupo<<endl;
+    cout << "Codigo de barras: "<< art.cod_barras<<endl;
+    cout<< "Nombre del articulo: "<< art.nom_articulo<<endl;
+    while (!art.depositos.esVacia()){
+        cout<<"Deposito nro "<<i<< ": "<<art.depositos.desencolar();
+        i++;
+    }
+}
+void imprimir_pila (Pila<articulo> a){
+    articulo art;
     while (!a.esVacia()){
-        cout << a.pop().nom_articulo << endl;
+        art= a.pop();
+        imprimir_articulo(art);
     }
 }
